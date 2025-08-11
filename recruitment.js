4cb1fd9f7564faf9edf8a6c616e70bb9dbe68a73
eirl-tea <riezkaluthfiyah@gmail.com>
2025-08-11 18:44:42 +0700

    Add files via upload

---
+++ recruitment.js
@@ -0,0 +1,426 @@
+
+const peserta = [
+      {
+        name: "Fajar Assiddiqi",
+        email: "Fajarsubuh@gmail.com",
+        phone: "081234567890",
+        class: "10 Z",
+        passed: false,
+        club: "MULTIMEDIA"
+      },
+      {
+        name: "Shane Michael Boose",
+        email: "nama@email.com",
+        phone: "081234567890",
+        class: "10 Z",
+        passed: true,
+        club: "D-GRAPH"
+      },
+      {
+        name: "Farah Alexandria",
+        email: "bulekimup@gmail.com",
+        phone: "0873912381109",
+        class: "10 M",
+        passed: true,
+        club: "ROBOTIK"
+      },
+      {
+        name: "Siti Naurah Firda Famelia Yusrinailah",
+        email: "ueue@gmail.com",
+        phone: "0873912381109",
+        class: "10 P",
+        passed: true,
+        club: "MEMBATIK"
+      },
+      {
+        name: "Ibrahim Raihan hadi putra",
+        email: "ibra67@gmail.com",
+        phone: "081933771390",
+        class: "10",
+        passed: true,
+        club: "MULTIMEDIA"
+      },
+      {
+        name: "MUHAMMAD GAOZHAN ALGHIFARI",
+        email: "gzhnalghifari@gmail.com",
+        phone: "085258718191",
+        class: "PLETON 2",
+        passed: false,
+        club: "ROBOTIK"
+      },
+      {
+        name: "Muhammad Santang Permana",
+        email: "permanamuhamad223@gmail.com",
+        phone: "085235680028",
+        class: "X",
+        passed: false,
+        club: "D'GRAPH"
+      },
+      {
+        name: "Anisah cahya kamilah",
+        email: "cahyakamilah1501@gmail.com",
+        phone: "085233676394",
+        class: "10 D",
+        passed: true,
+        club: "ROBOTIK"
+      },
+      {
+        name: "Ahmad Yani",
+        email: "ynii7232@gmail.com",
+        phone: "085233228825",
+        class: "10",
+        passed: false,
+        club: "D'GRAPH"
+      },
+      {
+        name: "Anggita Anggraini Wijaya",
+        email: "anggitaanggraini155@gmail.com",
+        phone: "089523935101",
+        class: "10 A",
+        passed: true,
+        club: "D'GRAPH"
+      },
+      {
+        name: "Muhammad Farih Albar",
+        email: "muhammadfarihalbar@gmail.com",
+        phone: "085727512326",
+        class: "10",
+        passed: true,
+        club: "D'GRAPH"
+      },
+      {
+        name: "NUNARINALHAQ",
+        email: "nunarinalh123@gmail.com",
+        phone: "085649855765",
+        class: "XI-I",
+        passed: false,
+        club: "D'GRAPH"
+      },
+      {
+        name: "Iqbal Zaky Akrom",
+        email: "iqbalzaky954@gmail.com",
+        phone: "081252170068",
+        class: "10",
+        passed: false,
+        club: "MULTIMEDIA"
+      },
+      {
+        name: "MUHAMAD NOVAL FERDIAN RISQULLA",
+        email: "novalferdianrizqulla041109@gmailm.com",
+        phone: "083846024154",
+        class: "X-H",
+        passed: true,
+        club: "MULTIMEDIA"
+      },
+      {
+        name: "MUHAMAD NOVAL FERDIAN RISQULLA",
+        email: "novalferdianrizqulla041109@gmail.com",
+        phone: "083846024154",
+        class: "10 H",
+        passed: true,
+        club: "MULTIMEDIA"
+      },
+      {
+        name: "maria azzahra tsabit rahman",
+        email: "mariaazzahrats@gmail.com",
+        phone: "085792405203",
+        class: "10 G",
+        passed: true,
+        club: "MULTIMEDIA"
+      },
+      {
+        name: "Ahmad Riffat jazmi asyhar",
+        email: "jazmiasyhar123@gmail.com",
+        phone: "087719320599",
+        class: "10",
+        passed: false,
+        club: "MULTIMEDIA"
+      },
+      {
+        name: "Muh Raihan Al Hafizh",
+        email: "bemcell79@gmail.com",
+        phone: "085182513584",
+        class: "Pleton 6 (VI)",
+        passed: false,
+        club: "INFORMATIKA"
+      },
+      {
+        name: "Muhammad syaiful rizal",
+        email: "muhammadrizal19294@gmail.com",
+        phone: "081907052797",
+        class: "10 F",
+        passed: false,
+        club: "MULTIMEDIA"
+      },
+      {
+        name: "Syifa afkarina alhadi",
+        email: "syifaafkarinaalhadi@gmail.com",
+        phone: "081995959218",
+        class: "10 A",
+        passed: true,
+        club: "D'GRAPH"
+      },
+      {
+        name: "Syifa afkarina al hadi",
+        email: "syifaafkarinaalhadi@gmail.com",
+        phone: "081995959218",
+        class: "10 A",
+        passed: true,
+        club: "D'GRAPH"
+      },
+      {
+        name: "Moch. Kevin Fiorello",
+        email: "mochkevinfio@gmail.com",
+        phone: "085233396347",
+        class: "10",
+        passed: false,
+        club: "MULTIMEDIA"
+      },
+      {
+        name: "Syifa Aydina Samsi",
+        email: "aydinoot@gmail.com",
+        phone: "085148459393",
+        class: "10 A",
+        passed: true,
+        club: "ROBOTIK"
+      },
+      {
+        name: "sultonul arifin",
+        email: "sultonulafin04@gmail.com",
+        phone: "085707198933",
+        class: "10 G",
+        passed: false,
+        club: "MULTIMEDIA"
+      },
+      {
+        name: "Ahmad aqil alhakim",
+        email: "ahmadaqil1420@gmail.com",
+        phone: "081333791789",
+        class: "10 F",
+        passed: false,
+        club: "MULTIMEDIA"
+      },
+      {
+        name: "Keisya Jingga Raturaka Suryanita",
+        email: "keisyapenaklukdunia@gmail.com",
+        phone: "081234794633",
+        class: "10 A",
+        passed: true,
+        club: "MULTIMEDIA"
+      },
+      {
+        name: "Kezia Alivia Dive",
+        email: "keziaaliviad@gmail.com",
+        phone: "087878833622",
+        class: "10 A",
+        passed: true,
+        club: "MULTIMEDIA"
+      },
+      {
+        name: "Ilham Ardhiansyah",
+        email: "ilhamarif392@gmail.com",
+        phone: "085807126921",
+        class: "10 A",
+        passed: true,
+        club: "INFORMATIKA"
+      },
+      {
+        name: "Zafron Rabbani Fauzi",
+        email: "zafronfauzi@gmail.com",
+        phone: "085797484134",
+        class: "10 A",
+        passed: true,
+        club: "D'GRAPH"
+      },
+      {
+        name: "Indah Wahyuni",
+        email: "indah280410@gmail.com",
+        phone: "087725906871",
+        class: "10 H",
+        passed: false,
+        club: "INFORMATIKA"
+      },
+      {
+        name: "ALIKA ALMIRATUS SHOLEHA",
+        email: "alikaalmiratus482@gmail.com",
+        phone: "082132177192",
+        class: "10 G",
+        passed: false,
+        club: "D'GRAPH"
+      },
+      {
+        name: "Muhamad Hilman rosidi",
+        email: "hilmanbds@gmail.com",
+        phone: "085809182770",
+        class: "10 A",
+        passed: true,
+        club: "ROBOTIK"
+      },
+      {
+        name: "Muhammad Yoni Mushlih",
+        email: "mmushlih80@gmail.com",
+        phone: "087736434295",
+        class: "10 A",
+        passed: true,
+        club: "ROBOTIK"
+      },
+      {
+        name: "M.Bayu Erfandra Putra Andri Firansyah",
+        email: "bkece047@gmail.com",
+        phone: "085655903627",
+        class: "10 B",
+        passed: false,
+        club: "MULTIMEDIA"
+      },
+      {
+        name: "M. Bayu Erfandra Putra Andri Firansyah",
+        email: "bkece047@gmail.com",
+        phone: "085655903627",
+        class: "10 B",
+        passed: false,
+        club: "MULTIMEDIA"
+      },
+      {
+        name: "Reyhan Dwi ardyansa",
+        email: "reyhandwiardyansa829@gmail.com",
+        phone: "082143636570",
+        class: "10 H",
+        passed: false,
+        club: "D'GRAPH"
+      },
+      {
+        name: "Najwa Putri Maulida As-Syaif",
+        email: "najwajahwa97@mail.com",
+        phone: "085235540155",
+        class: "10 G",
+        passed: true,
+        club: "INFORMATIKA"
+      },
+      {
+        name: "Queen nabila khoirunnisa",
+        email: "queenssaa09@gmail.com",
+        phone: "085855055076",
+        class: "10 C",
+        passed: true,
+        club: "D'GRAPH"
+      },
+      {
+        name: "Muhammad Zidan Latif",
+        email: "zidanlatif321@gmail.com",
+        phone: "085704081369",
+        class: "10 F",
+        passed: true,
+        club: "INFORMATIKA"
+      },
+      {
+        name: "Nadiyah Zian Walidah",
+        email: "zianwalidahnadiah@gmail.com",
+        phone: "08971690601",
+        class: "10 E",
+        passed: true,
+        club: "MULTIMEDIA"
+      },
+      {
+        name: "Lutvania Indana Zulfa",
+        email: "lutvaniaindanazulfa@gmail.com",
+        phone: "085816223686",
+        class: "10 D",
+        passed: false,
+        club: "MULTIMEDIA"
+      },
+      {
+        name: "Naimma Kahila Lubna",
+        email: "kahilalubnanaima@gmail.com",
+        phone: "087729197434",
+        class: "10 C",
+        passed: true,
+        club: "D'GRAPH"
+      },
+      {
+        name: "Refifa Hanzayyani",
+        email: "haniuntung28@gmail.com",
+        phone: "081252695079",
+        class: "10 H",
+        passed: false,
+        club: "MULTIMEDIA"
+      },
+      {
+        name: "Aurelia Takhsya Nabila",
+        email: "nabilatata307@gmail.com",
+        phone: "089529604131",
+        class: "10 E",
+        passed: false,
+        club: "D'GRAPH"
+      },
+      {
+        name: "Najwa Rahmawati",
+        email: "najwarahmawati2025@gmail.com",
+        phone: "085645065367",
+        class: "10 D",
+        passed: false,
+        club: "MULTIMEDIA"
+      },
+      {
+        name: "Vienika Hanum Masruro",
+        email: "hanumcantik1212@gmail.com",
+        phone: "084138513422",
+        class: "10 D",
+        passed: false,
+        club: "D'GRAPH"
+      },
+      {
+        name: "muhammad najih arrusydi",
+        email: "najiharusydi@gmail.com",
+        phone: "082128548340",
+        class: "XI B",
+        passed: false,
+        club: "MULTIMEDIA"
+      },
+      {
+        name: "Rosifa Dwi Febrianta",
+        email: "a@gmail.com",
+        phone: "085852075486",
+        class: "10 F",
+        passed: true,
+        club: "INFORMATIKA"
+      },
+      {
+        name: "Wildan Muktafi Nabil",
+        email: "wildanmuktafinabil@gmail.com",
+        phone: "081249048750",
+        class: "10 H",
+        passed: true,
+        club: "INFORMATIKA"
+      },
+      {
+        name: "Ahmad Aqil Alhakim",
+        email: "ahmadaqil1420@gmail.com",
+        phone: "081333791789",
+        class: "10 F",
+        passed: true,
+        club: "MULTIMEDIA"
+      }
+    ];
+
+    function checkResult() {
+      const name = document.getElementById("nama").value.trim().toLowerCase();
+      const email = document.getElementById("email").value.trim().toLowerCase();
+        
+      const result = peserta.find(p =>
+        p.name.toLowerCase() === name && p.email.toLowerCase() === email
+      );
+
+      if (result) {
+        if (result.passed) {
+          localStorage.setItem("dataPeserta", JSON.stringify(result))
+          window.location.href = "biru.html";
+        } else {
+          localStorage.setItem("dataPeserta", JSON.stringify(result))
+          window.location.href = "merah.html";
+        }
+      } else {
+        alert("Data tidak ditemukan. Pastikan nama dan email sesuai saat pendaftaran.");
+      }
+    }
+
+
